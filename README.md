440eba00bba0f0323017be72df807749e4d6dbb7
Antonio Piga <antonio.piga93@gmail.com>
2024-01-12 18:41:19 +0100

    feat: first version

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # news-api-middleware
 This repository serves as a middleware between https://newsapi.ai and a GPT application, allowing the GPT application to make calls to it
+## how to use
+You need an API key from newsapi.ai, which must be inserted into the .env file.
+### deploy
+To facilitate deployment, you require a Nuvolaris environment. Simply type 'task deploy' to build this Python actions into it.
+
+
+
+
---
+++ packages/news/news.py
@@ -0,0 +1,33 @@
+import json
+from urllib import request
+
+def main(args):
+    url = 'http://eventregistry.org/api/v1/event/getBreakingEvents'
+    headers = {'Content-Type': 'application/json'}
+    data = {
+        "eventImageCount": 3,
+        "breakingEventsCount": 1,
+        "includeEventSummary": True,
+        "includeEventLocation": False,
+        "includeEventSocialScore": False,
+        "includeEventArticleCounts": False,
+        "includeEventConcepts": False,
+        "includeEventCategories": False,
+        "includeConceptLabel": False,
+        "includeEventSentiment": False,
+        "includeLocationGeoLocation": False,
+        "apiKey": args.get('API_KEY')
+    }
+
+    data = json.dumps(data).encode('utf-8')
+
+    req = request.Request(url, data=data, headers=headers)
+
+    try:
+        with request.urlopen(req) as response:
+            result = response.read().decode('utf-8')
+            return {
+                'body': json.loads(result)
+            }
+    except request.URLError as e:
+        return f"Error: {e}"
